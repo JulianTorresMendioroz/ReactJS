ec66c985ea8b94ed52c10d3fb89907c05f7e4d2b
JulianTorresMendioroz <juliantorresmendioroz348@gmail.com>
2023-01-03 23:58:33 -0300

    contador terminado, falta acomodar boton disabled y stock

--- src/components/ItemCount.js
+++ src/components/ItemCount.js
@@ -3,18 +3,16 @@ import { useState } from 'react'
 
 const ItemCount = (props) => {
 
-let {stock, initial, string} = props
+let {stock, initial} = props
 
-const [counter,setCounter] = useState(initial)
+const [counter,setCounter] = useState(parseInt(initial))
 
 let increase = () => {
 
- initial =  setCounter(counter + 1) 
+ setCounter(counter + 1) 
  console.log(setCounter)
-  
 
 }
-
 
 let decrease = () => {
 
@@ -23,13 +21,34 @@ console.log(setCounter)
 
 }
 
+if(counter > 5) {
+
+  alert('No hay mas stock disponible')
+
+
+}
+
+const [disabled,setDisabled] = useState(true)
+
+if(disabled == true) {
+
+  setDisabled(decrease())
+
+}
+
+
+
+
+
+
+
 
   return (
     <>
 
 <button onClick={increase}>+</button>
 <span>{counter}</span>
-<button onClick={decrease}>-</button>
+<button disabled={true} onClick={decrease}>-</button>
 <button>Agregar al carrito</button>
 
 
